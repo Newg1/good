c299df9b2cf7e9aaf349cecf8d9302f29fbc29f2
Newg1 <107351986+Newg1@users.noreply.github.com>
2022-08-18 20:01:37 +0900

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2213,7 +2213,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
+		+ ' | Port by Newbion | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
